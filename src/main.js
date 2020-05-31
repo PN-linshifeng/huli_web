d58f36677d3135bf2eea5ab1117262bb241a7ca2
Holo <1984374030@qq.com>
2020-05-31 23:02:56 +0800

    联系我们添加百度地图

--- src/main.js
+++ src/main.js
@@ -6,13 +6,11 @@ import 'element-ui/lib/theme-chalk/display.css';
 import router from './router/index'; // eslint-disable-line
 import App from './App';
 import store from './store/index';
-import AMap from 'vue-amap'; // 高德地图组件
 import './assets/font/iconfont.css';
 
 // style
 import './assets/scss/style.scss';
 
-Vue.use(AMap);
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
@@ -26,10 +24,3 @@ new Vue({
   render: h => h(App),
 }).$mount('#app');
 
-// 初始化vue-amap
-AMap.initAMapApiLoader({
-  // 高德key
-  key: 'baa309a4fcaf0e15243611acf162d558',
-  // 插件集合 （插件按需引入）
-  plugin: ['AMap.Geolocation']
-})
--- src/views/Contractus/Index.vue
+++ src/views/Contractus/Index.vue
@@ -4,26 +4,17 @@
       <h3>联系我们</h3>
     </div>
     <!-- 地图 -->
-    <div class="map-view hidden-sm-and-up">
-      <el-amap vid="amap" :plugin="plugin" class="amap-demo" :center="center" />
-    </div>
+    <div id="allmap" ref="allmap" class="hidden-sm-and-up" />
     <div class="contract-layout">
       <div class="content">
         <!-- 基本信息 -->
-        <div class="base-msg hidden-xs-only">
+        <div class="base-msg">
           <h3><span class="red-text">一</span> 联系方式</h3>
           <h4>广西狐狸文化传媒有限公司</h4>
           <p>地址：广西壮族自治区南宁市东盟财经广场1号楼601</p>
           <p>邮编：10085</p>
           <p>邮箱：hr@hulimedia.com</p>
-          <el-divider />
-        </div>
-        <div class="base-msg hidden-sm-and-up">
-          <h3><span class="red-text">一</span> 联系方式</h3>
-          <h4>广西狐狸文化传媒有限公司</h4>
-          <p><span>ADDRESS</span>广西壮族自治区南宁市东盟财经广场1号楼601</p>
-          <p><span>PHONE</span>010-000000</p>
-          <p><span>E-MAIL</span>hr@hulimedia.com</p>
+          <el-divider class="hidden-xs-only" />
         </div>
         <!-- 联系人信息 -->
         <el-row class="hidden-xs-only">
@@ -49,7 +40,6 @@ import './style.scss';
 
 export default {
   data() {
-    const self = this;
     return {
       contractMsg: [
         {
@@ -80,34 +70,26 @@ export default {
           tel: '14523335666',
           Email: 'hr@hulimedia.com'
         }
-      ],
-      center: [108.266119, 22.83916],
-      lng: 0,
-      lat: 0,
-      loaded: false,
-      plugin: [{
-        timeout: 100, // 超过10秒后停止定位，默认：无穷大
-        showMarker: true, // 定位成功后在定位到的位置显示点标记，默认：true
-        showCircle: false, // 定位成功后用圆圈表示定位精度范围，默认：true
-        panToLocation: true, // 定位成功后将定位到的位置作为地图中心点，默认：true
-        extensions: 'all',
-        pName: 'Geolocation',
-        events: {
-          init(o) {
-            // o 是高德地图定位插件实例
-            o.getCurrentPosition((status, result) => {
-              console.log(result)
-              if (result && result.position) {
-                self.lng = result.position.lng;
-                self.lat = result.position.lat;
-                self.center = [self.lng, self.lat];
-                self.loaded = true;
-                self.$nextTick();
-              }
-            });
-          }
-        }
-      }]
+      ]
+    }
+  },
+  mounted() {
+    this.map();
+  },
+  methods: {
+    map() {
+      const map = new window.BMap.Map(this.$refs.allmap) // 创建Map实例
+      map.centerAndZoom(new window.BMap.Point(108.27255, 22.845113), 16) // 初始化地图,设置中心点坐标和地图级别
+      map.addControl(new window.BMap.MapTypeControl({ // 添加地图类型控件
+        mapTypes: [
+          window.BMAP_NORMAL_MAP,
+          window.BMAP_HYBRID_MAP
+        ]
+      }))
+      const marker = new window.BMap.Marker(new window.BMap.Point(108.27255, 22.845113));
+      map.addOverlay(marker);
+      map.setCurrentCity('南宁') // 设置地图显示的城市 此项是必须设置的
+      map.enableScrollWheelZoom(true)// 开启鼠标滚轮缩放
     }
   }
 }
